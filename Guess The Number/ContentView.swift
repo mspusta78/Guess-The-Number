b918dd6c3d01114986c722d4297399eefec4af26
Martin Spusta <martinspusta@gmail.com>
2021-02-14 22:03:18 -0500

    Initial Commit

---
+++ Guess The Number/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess The Number
+//
+//  Created by Martin Spusta on 2/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guess The Number/Guess_The_NumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_NumberApp.swift
+//  Guess The Number
+//
+//  Created by Martin Spusta on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_NumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
